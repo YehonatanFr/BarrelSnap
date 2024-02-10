60c56341203ffadd81cf19ee380037ae45c5e7ea
YehonatanFr <yonatanfr10@gmail.com>
2024-02-10 21:06:55 +0200

    add dependecy

--- barrelsnap01/lib/pages/wrapper.dart
+++ barrelsnap01/lib/pages/wrapper.dart
@@ -5,8 +5,6 @@ import 'package:startertemplate/pages/home.dart';
 class Wrapper extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-
     // return either Home - client or business, or Authenticate widget
     return Home();
   }
