54f1c53218630d53837419cf05bbf4a844c7e470
YehonatanFr <yonatanfr10@gmail.com>
2024-02-18 11:34:59 +0200

    Add Readme

--- barrelsnap01/lib/main.dart
+++ barrelsnap01/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:BarrelSnap/pages/login_page.dart'; // Import LoginPage
+import 'package:BarrelSnap/pages/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(), // Set LoginPage as the home screen directly
+    return const MaterialApp(
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
